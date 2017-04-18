556e56c28880d17474c3fae6db405da5d7dd054d
mostafa <mostafa@email.com>
2017-04-18 14:54:40 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo}     = require('./models/todo');
 var {User}     = require('./models/user');
@@ -26,7 +26,25 @@ app.get('/todos', (req, res) => {
 	Todo.find().then( (todos) => {
 		res.send({todos});
 	}, (e) => {
-		res.status(400).send(e);
+		res.status(400).send(e); 
+	});
+});
+
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then( (todo) => {
+		if(!todo) {
+			res.status(404).send();
+		}
+		res.send({todo});
+	}, (e) => {
+		res.status(404).send();
 	});
 });
 
