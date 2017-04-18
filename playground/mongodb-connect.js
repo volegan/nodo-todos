d24c6b763a2910069e3be90d88ab40f9bad4ab8f
mostafa <mostafa@email.com>
2017-04-18 11:25:29 +0200

    Add POST /todos route and refactor mongoose

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,4 @@
-//const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;  property destructing
 const {MongoClient, ObjectID} = require('mongodb');
 
 
