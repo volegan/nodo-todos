6ecb468a615cada4ff6281d9398e23a0eb144391
mostafa <mostafa@email.com>
2017-04-22 20:27:06 +0200

    Add test fpr POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -264,3 +264,57 @@ describe('POST /users', () => {
 	});
 
 });
+
+
+
+describe('POST /users/login', () => {
+	it('should login user and return auth token' , (done) => {
+		request(app)
+		  .post('/users/login')
+		  .send({
+		  	email: user[1].email,
+		  	password: user[1].password
+		  })
+		  .expect(200)
+		  .expect( (res) => {
+		  	expect(res.header['x-auth']).toExist();		
+		  })
+		  .end( (err, res) => {
+		  	if(err) {
+		  		return done(err);
+		  	}
+
+		  	User.findById(user[1]._id).then((user) => {
+		  		expect(user.tokens[0]).toInclude({
+		  			access: 'auth',
+		  			token: res.header['x-auth']
+		  		});
+
+		  		done();	
+		  	}).catch((e) => done(e));
+		  });	
+	});
+
+	it('should reject invalid login', (done) => {
+		request(app)
+		  .post('/users/login')
+		  .send({
+		  	email: user[1].email,
+		  	password: user[1].password + '1'
+		  })
+		  .expect(400)
+		  .expect( (res) => {
+		  	expect(res.header['x-auth']).toNotExist();		
+		  })
+		  .end( (err, res) => {
+		  	if(err) {
+		  		return done(err);
+		  	}
+
+		  	User.findById(user[1]._id).then((user) => {
+		  		expect(user.tokens.length).toBe(0);
+		  		done();	
+		  	}).catch((e) => done(e));
+		  });		
+	});
+});
