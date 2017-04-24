7c39b76e3aedc0c5453198505775b98c25ff51f1
mostafa <mostafa@email.com>
2017-04-24 11:30:06 +0200

    Add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -318,3 +318,23 @@ describe('POST /users/login', () => {
 		  });		
 	});
 });
+
+describe('DELETE /users/me/token', () => {
+	it('should remove auth token on logout', (done) => {
+		request(app)
+			.delete('/users/me/token')
+			.set('x-auth', user[0].tokens[0].token)
+			.expect(200)
+			.end((err, res) => {
+				if(err) {
+					return done(err);
+				}
+
+				User.findById(user[0]._id).then((user) => {
+					expect(user.tokens.length).toBe(0);
+					done();
+				}).catch((e) => done(e));
+			})
+
+	});
+});
